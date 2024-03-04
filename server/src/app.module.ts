5af6e5adf32f6bb5b6ce947c78c2e9c8dd630e47
Mirla Braga <mirla.chucre@dft.gov.uk>
2024-03-04 09:57:54 +0000

    Created the GET /clinics endpoint for get all clinics with tests

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClinicsController } from './clinics/clinics.controller';
+import { ClinicsModule } from './clinics/clinics.module';
+import { PatientsService } from './patients/patients.service';
+import { PatientsModule } from './patients/patients.module';
+import { ClinicsService } from './clinics/clinics.service';
+import { UtilsService } from './utils/utils.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ClinicsModule, PatientsModule],
+  controllers: [AppController, ClinicsController],
+  providers: [AppService, ClinicsService, PatientsService, UtilsService],
 })
 export class AppModule {}
