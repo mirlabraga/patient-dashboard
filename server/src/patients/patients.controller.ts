8e41d38c85e21c6d6f79f3d7b9c43df4e2a5e3d2
Mirla Braga <mirla.chucre@dft.gov.uk>
2024-03-05 00:18:28 +0000

    Updated the GET /patients endpoint for support query parameter with clinic id

--- server/src/patients/patients.controller.ts
+++ server/src/patients/patients.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { Patient, PatientsService } from './patients.service';
 
 @Controller('patients')
 export class PatientsController {
   constructor(private readonly patientsService: PatientsService) {}
 
-  @Get()
-  async getPatientByClinic(clinicId: number | undefined): Promise<Patient[]> {
-    return await this.patientsService.getPatientByClinic(clinicId);
+  @Get(':id')
+  async getPatientByClinic(
+    @Param('id') id: number | undefined,
+  ): Promise<Patient[]> {
+    return await this.patientsService.getPatientByClinic(id);
   }
 }
